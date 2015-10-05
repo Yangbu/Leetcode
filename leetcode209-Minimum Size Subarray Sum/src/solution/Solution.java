8f84bb2d707b2e0082b5f7a42fe0865353eb33f3
Yang <wuyangbu@bu.edu>
2015-10-05 10:18:22 -0400

    leetcode

---
+++ leetcode209-Minimum Size Subarray Sum/src/solution/Solution.java
@@ -0,0 +1,15 @@
+package solution;
+//O(n)
+
+public class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        int start = 0, end = 0, sum = 0, minLen = Integer.MAX_VALUE;
+        while (end < nums.length) {
+            while (end < nums.length && sum < s) sum += nums[end++];
+            if (sum < s) break;
+            while (start < end && sum >= s) sum -= nums[start++];
+            if (end - start + 1 < minLen) minLen = end - start + 1;
+        }
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
+}
