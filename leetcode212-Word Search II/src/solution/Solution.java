0dcfaa79821d22b15d7c1a17a60bd43dd1a5bddf
Yang <wuyangbu@bu.edu>
2015-10-08 20:50:55 -0400

    leetcode

---
+++ leetcode212-Word Search II/src/solution/Solution.java
@@ -0,0 +1,105 @@
+package solution;
+/*
+ * 
+ * Compared with Word Search, I make my DFS with a tire but a word. 
+ * The Trie is formed by all the words in given words. Then during the DFS, 
+ * for each current formed word, I check if it is in the Trie.
+ */
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+    Set<String> res = new HashSet<String>();
+
+    public List<String> findWords(char[][] board, String[] words) {
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.insert(word);
+        }
+
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dfs(board, visited, "", i, j, trie);
+            }
+        }
+
+        return new ArrayList<String>(res);
+    }
+
+    public void dfs(char[][] board, boolean[][] visited, String str, int x, int y, Trie trie) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) return;
+        if (visited[x][y]) return;
+
+        str += board[x][y];
+        if (!trie.startsWith(str)) return;
+
+        if (trie.search(str)) {
+            res.add(str);
+        }
+
+        visited[x][y] = true;
+        dfs(board, visited, str, x - 1, y, trie);
+        dfs(board, visited, str, x + 1, y, trie);
+        dfs(board, visited, str, x, y - 1, trie);
+        dfs(board, visited, str, x, y + 1, trie);
+        visited[x][y] = false;
+    }
+}
+class TrieNode {
+    // Initialize your data structure here.
+    public boolean isWord;
+    public TrieNode[] children = new TrieNode[26];
+    public TrieNode() {
+        
+    }
+}
+
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+        TrieNode cur = root;
+        for(int i=0; i<word.length(); i++){
+            char c = word.charAt(i);
+            if(cur.children[c - 'a'] == null){
+                cur.children[c - 'a'] = new TrieNode();
+            }
+            cur = cur.children[c-'a'];
+        }
+        cur.isWord = true;
+    }
+
+    // Returns if the word is in the trie.
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for(int i=0; i<word.length(); i++){
+            char c = word.charAt(i);
+            if(cur.children[c - 'a'] == null) return false;
+            cur = cur.children[c - 'a'];
+        }
+       return cur.isWord; 
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for(int i=0; i<prefix.length(); i++){
+            char c = prefix.charAt(i);
+            if(cur.children[c - 'a'] == null) return false;
+            cur = cur.children[c - 'a'];
+        }
+        return true;
+    }
+}
