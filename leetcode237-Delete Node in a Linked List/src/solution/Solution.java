0598708f8fe26007a33ff6358ddb9b985640c9a9
Yang <wuyangbu@bu.edu>
2015-10-19 21:25:36 -0400

    leetcode

---
+++ leetcode237-Delete Node in a Linked List/src/solution/Solution.java
@@ -0,0 +1,10 @@
+package solution;
+
+public class Solution {
+    public void deleteNode(ListNode node) {
+        if(node !=null && node.next != null){
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
