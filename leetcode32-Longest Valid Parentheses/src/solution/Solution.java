327170ad00a80e64ed6223b22f83b55b57ac3e16
Yang <wuyangbu@bu.edu>
2015-11-06 11:47:35 -0500

    leetcode

---
+++ leetcode32-Longest Valid Parentheses/src/solution/Solution.java
@@ -0,0 +1,21 @@
+package solution;
+
+import java.util.Stack;
+
+public class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int res=0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                stack.push(i);
+            }else if(!stack.isEmpty() && s.charAt(stack.peek())=='('){
+                stack.pop();
+                res = Math.max((stack.isEmpty()? i+1 : i-stack.peek()),res);
+            }else {
+                stack.push(i);
+            }
+        }
+        return res;
+    }
+}
