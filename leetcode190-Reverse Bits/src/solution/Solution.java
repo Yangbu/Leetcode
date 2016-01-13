3fe6dec1c1e1f9dda9e243a5dbf0deb57e0618b2
Yang <wuyangbu@bu.edu>
2016-01-12 19:39:25 -0500

    leetcode

---
+++ leetcode190-Reverse Bits/src/solution/Solution.java
@@ -0,0 +1,12 @@
+package solution;
+
+public class Solution {
+    int result = 0;
+    for (int i = 0; i < 32; i++) {
+        result += n & 1;
+        n >>>= 1;   // CATCH: must do unsigned shift
+        if (i < 31) // CATCH: for last digit, don't shift!
+            result <<= 1;
+    }
+    return result;
+}
